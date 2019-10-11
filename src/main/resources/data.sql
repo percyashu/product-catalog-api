f8d7a0bf2e57e64758c79e5848fd9559223ec019
percyashu <percyayukashu@gmail.com>
2019-10-11 16:26:28 +0100

    Configured eDb

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,11 +5,11 @@ INSERT INTO category_tbl VALUES(3, 'Vehicles and Garage');
 INSERT INTO category_tbl VALUES(4, 'Cloths and Shoes');
 INSERT INTO category_tbl VALUES(5, 'Food and Nutrition');
 
-INSERT INTO product_tbl  VALUES(1, 'MacBook Pro', 10, 2000000,1);
-INSERT INTO product_tbl  VALUES(2, 'Airfresher', 8, 20000,3);
-INSERT INTO product_tbl  VALUES(3, 'T-Shirt', 10, 4000,4);
-INSERT INTO product_tbl  VALUES(4, 'Water Filter', 10, 10000,5);
-INSERT INTO product_tbl  VALUES(5, 'Air Conditional', 10, 50000,2);
-INSERT INTO product_tbl  VALUES(6, 'Hp Pavillion', 10, 2000000,1);
+INSERT INTO product_tbl  VALUES(1, 'MacBook Pro', 2000000, 10,1);
+INSERT INTO product_tbl  VALUES(2, 'Airfresher', 20000, 8,3);
+INSERT INTO product_tbl  VALUES(3, 'T-Shirt',  4000,10,4);
+INSERT INTO product_tbl  VALUES(4, 'Water Filter', 10000,10 ,5);
+INSERT INTO product_tbl  VALUES(5, 'Air Conditional', 50000,10 ,2);
+INSERT INTO product_tbl  VALUES(6, 'Hp Pavillion', 2000000, 10,1);
 
 
